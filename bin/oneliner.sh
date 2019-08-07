83a7e24de3e8661133a51b085cc0238c467db1f3
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2019-08-07 15:13:28 +1200

    Add note

--- bin/oneliner.sh
+++ bin/oneliner.sh
@@ -2,4 +2,5 @@
 curl -sSL https://raw.githubusercontent.com/extdn/installer-m2/master/build/extdn_installer.phar -o extdn_installer.phar
 chmod +x extdn_installer.phar
 ./extdn_installer.phar $@
-rm -i extdn_installer.phar
+echo "The ExtDN installer was downloaded into the current directory"
+echo "You can delete it by running rm -i extdn_installer.phar"
