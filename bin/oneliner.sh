64258b6781a15d59e3500bfdf4d2b61af93cf823
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2019-08-07 16:11:30 +1200

    Keep return value of phar execution

--- bin/oneliner.sh
+++ bin/oneliner.sh
@@ -2,6 +2,9 @@
 curl -sSL https://raw.githubusercontent.com/extdn/installer-m2/master/build/extdn_installer.phar -o extdn_installer.phar
 chmod +x extdn_installer.phar
 ./extdn_installer.phar $@
+RET_VALUE=$?
 echo ""
 echo "The ExtDN installer was downloaded into the current directory"
 echo "You can delete it by running rm -i extdn_installer.phar"
+
+exit ${RET_VALUE}
