bcae9d40d4be6c59737bfa2d234d0d416d7d16b6
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2019-08-07 21:48:34 +1200

    Also clear static content

--- src/Task/ModuleEnable.php
+++ src/Task/ModuleEnable.php
@@ -41,7 +41,7 @@ class ModuleEnable extends BaseBinMagento
         if (empty($moduleDiff)) {
             return Result::error($this, 'No new modules detected.');
         }
-        $this->modulesToEnable = '';
+        $this->modulesToEnable = ' --clear-static-content';
         foreach ($moduleDiff as $module => $value) {
             $this->modulesToEnable .= ' ' . $module;
         }
